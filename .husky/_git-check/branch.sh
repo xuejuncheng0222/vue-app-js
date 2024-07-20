552f4063dffa8defa690a6b58712580beba29da2
薛俊成 <xuejuncheng@msunhealth.com>
2024-07-21 02:06:42 +0800

    feat: 测试格式化代码

--- .husky/_git-check/branch.sh
+++ .husky/_git-check/branch.sh
@@ -8,6 +8,6 @@ pattern="^((fix|feat)-[a-z]{2,}-([a-zA-Z0-9]+)|main|master|release-[a-zA-Z0-9-]+
 
 # 正则不匹配就错误提示
 if [[ ! $branch_name =~ $pattern ]]; then
-  echo "Error: Invalid branch name. Branch name must match '(fix|feat)-(名字缩写)-(具体改动如addcoupon)' format."
+  echo "Error: Invalid branch name. Branch name must match '(fix|feat)-(名字缩写)-(具体改动) , main , master ,release-(功能分支) ' format."
   exit 1
 fi
