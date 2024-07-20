299a657c15c55483ce4a103323169bee8366f8a2
薛俊成 <xuejuncheng@msunhealth.com>
2024-07-21 01:58:08 +0800

    feat: 提交对分支名称的校验

---
+++ .husky/_git-check/branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 获取当前所在的 Git 分支名
+branch_name=$(git symbolic-ref --short HEAD)
+
+# 分支名规则
+pattern="^((fix|feat)-[a-z]{2,}-([a-zA-Z0-9]+)|main|master|release-[a-zA-Z0-9-]+)$"
+
+# 正则不匹配就错误提示
+if [[ ! $branch_name =~ $pattern ]]; then
+  echo "Error: Invalid branch name. Branch name must match '(fix|feat)-(名字缩写)-(具体改动如addcoupon)' format."
+  exit 1
+fi
